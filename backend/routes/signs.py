75c2124ea169645c27c0a97a45c4ad26a4b8a220
Bilal Ishaq <bilal.developer05@gmail.com>
2026-03-30 22:55:27 +0500

    Update

--- backend/routes/signs.py
+++ backend/routes/signs.py
@@ -3,7 +3,7 @@ from database import db
 
 router = APIRouter(prefix="/signs", tags=["Traffic Signs"])
 
-# Remove trailing slash in route to avoid 307 redirect
+
 @router.get("")
 def get_signs():
     # Fetch all signs from MongoDB and exclude '_id'
